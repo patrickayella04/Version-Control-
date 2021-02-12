cb230168362d096c0a22701169b125d636d4fa74
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-02-12 06:42:36 +0000

    code

--- src/components/atoms/nav-atoms/Nav-links.js
+++ src/components/atoms/nav-atoms/Nav-links.js
@@ -5,20 +5,27 @@ import { Link } from 'react-router-dom';
 
 
 function NavLinks() {
-    const[open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    
+    
+
+    
     return (
         
         <>
             <ul className="nav-links"style={{ transform: open ? "translateX(0px)" : "" }}>
                 
-                <Link onClick={() => setOpen(!open)} to='/'>
-                <li>About</li>
-                </Link>
-                <Link onClick={() => setOpen(!open)} to='/about' >
-                <li>Blog</li>
+                
+                
+                <Link onClick={() => setOpen(!open)}  to='/about'  >
+                    <li>Blog</li> 
                 </Link> 
+                    
+                
+                
                 <Link onClick={() => setOpen(!open)} to='/contact' >
-                <li className="no-border">Contact</li>
+
+                    <li>Contact</li>
                 </Link>               
         </ul> 
         
