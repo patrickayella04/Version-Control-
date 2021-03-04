1c8316394f8da2cf95be4bb81ba01f8de1b3f102
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2021-03-04 13:33:16 +0000

    code

--- src/components/atoms/project-atoms/Project-text.js
+++ src/components/atoms/project-atoms/Project-text.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import pic from './Picture/contactKeeper.jpg';
 import picGit from './Picture/gitHubFinder.jpg';
+import picWeather from './Picture/Screenshot(7).png';
 
 
 function ProjectText() {
@@ -12,6 +13,18 @@ function ProjectText() {
             
             
             <ul className="vlog-list">
+
+            <li>
+                    <a className="link-title" href="https://dazzling-johnson-b9c1d5.netlify.app/">
+                        View: Weather project!
+                         </a>
+                    <br />
+                        Use this application to find out what weather will be!
+                        <br />
+                    <img className="picture" src={picWeather} alt="" />
+                    <p>View Code: <a className="link-title" href="https://github.com/patrickayella04/Weather-and-News"> Weater-and-News </a></p>
+                </li>
+            
 
                 <li >
                     
